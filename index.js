12ed22237946808cc1110f91337fb184d0160624
Dayanne Alcantara <dayannealcantara14@gmail.com>
2021-08-23 23:48:15 -0300

    index.js do projeto

---
+++ index.js
@@ -0,0 +1,11 @@
+import { cliente } from "./cliente.js";
+import {contaCorrente} from "./contaCorrente.js";
+import { contaPoupanca } from "./contaPoup.js";
+
+const cliente1 = new cliente("Ana", 22255588836);
+const contaCorrenteAna = new contaCorrente(cliente1, 1001);
+const contaPoupanca1 = new contaPoupanca1 (50, cliente1, 1001);
+
+  console.log(contaPoupanca);
+  console.log(contaCorrenteAna);
+  
